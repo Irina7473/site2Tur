8b708bb81e08e12467fc736a31e8e6b6b9ac5947
Irina <ten7473@mail.ru>
2022-05-29 11:18:47 +0500

    Немного рефакторинга

--- pages/login.php
+++ pages/login.php
@@ -3,29 +3,32 @@ if (isset($_SESSION['ruser'])) {   ?>
     <form action="index.php"
     <?php  if (isset($_GET['page'])) echo '?page=' . $_GET['page'];  ?>
         class="form-inline pull-right" method="post">
-        <h4>Hello, <span>  <?php $_SESSION['ruser'] ?>  </span>
-    <input type="submit" value="Logout" id="ex" name="ex" class="btn btn-default btn-xs"></h4>
-</form>
+        <h4>Привет, <span>  <?php echo $_SESSION['ruser'] ?>  </span>
+        <input type="submit" value="Выйти" id="exit" name="exit" class="btn btn-default btn-xs"></h4>
+    </form>
 
-<?php
-if (isset($_POST['ex'])) {
-    unset($_SESSION['ruser']);
-    unset($_SESSION['radmin']);
-    unset($_SESSION['rfill']);
-    echo '<script>window.location.reload()</script>';
-}
+    <?php
+    if (isset($_POST['exit'])) {
+        unset($_SESSION['ruser']);
+        unset($_SESSION['radmin']);
+        unset($_SESSION['rfill']);
+        echo '<script>window.location.reload()</script>';
+    }
 } else {
     if (isset($_POST['press'])) {
         if (login($_POST['login'], $_POST['pass'])) {
             echo '<script>window.location.reload()</script>';
         }
     } else {
-        echo '<form action="index.php';
+        ?>
+        <form action="index.php
+        <?php
         if (isset($_GET['page'])) echo '?page=' . $_GET['page'];
-        echo '" class="input-group input-group-sm pull-right" method="post">';
-        echo '<input type="text" name="login" size="10" class="" placeholder="login">
-    <input type="password" name="pass" size="10" class="" placeholder="password">
-    <input type="submit" id="press" value="Login" class="btn btn-default btn-xs" name="press">
-</form>';
-    }
+        ?>
+        " class="input-group input-group-sm pull-right" method="post">
+        <input type="text" name="login" size="10" class="" placeholder="логин">
+        <input type="password" name="pass" size="10" class="" placeholder="пароль">
+        <input type="submit" id="press" value="Войти" class="btn btn-default btn-xs" name="press">
+    </form>
+   <?php }
 }
